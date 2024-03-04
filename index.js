5db9df8c84873ccf23f8f3ba96a5daa486769bd9
dev-jbkang <jbin8797@gmail.com>
2024-03-04 16:00:01 +0900

    feat: 네비게이션 페이지 렌더링 연결

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("app");
+import Home from "./src/pages/Home";
+import About from "./src/pages/About";
+import Item from "./src/pages/Item";
+
+const $app = document.getElementById("app");
+const routes = {
+  "/": Home,
+  "/about": About,
+  "/item": Item,
+};
+const { pathname } = window.location;
+const Component = routes[pathname];
+
+$app.innerHTML = Component();
